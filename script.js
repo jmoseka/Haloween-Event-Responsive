a613aa17c29d94eb4a4b4e798c51f60442b541a3
jmoseka <jamylam3@gmail.com>
2022-11-14 19:24:17 +0300

    Add scroll effect

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ const headerEl = document.querySelector(".main-header");
 btnNavEl.addEventListener('click', function() {
     headerEl.classList.toggle('nav-open');
 })
+
+/** SMOOTH SCROLLING */
+/** all element with href property */
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach((link) => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = link.getAttribute("href")
+    
+        
+    // scroll back to the top
+    if(href === "#") window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+
+    // scrol to other thinks
+
+    if (href !== "#" && href.startsWith("#")) {
+        const sectionEl =  document.querySelector(href);
+        sectionEl.scrollIntoView({behavior: 'smooth'})
+    }
+    })
+
+    // close mobile nav
+    console.log(link.classList)
+
+    // if(link.classList.contains('nav-link')) {
+    //     console.log('yes')
+    //     headerEl.classList.toggle('nav-open');
+    // }
+})
